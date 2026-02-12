00bde99a6efbf18e350101a6ea4ac69d13da7bd8
Ismael Benlarbi <ismael.benlarbi@gmail.com>
2026-02-12 17:45:21 +0100

    update

--- config/db.php
+++ config/db.php
@@ -1,13 +1,15 @@
 <?php
-$host = 'localhost';
-$dbname = 'vite_et_gourmand';
-$username = 'root';
-$password = '';
+// Informations de connexion Alwaysdata
+$host     = 'mysql-vite-et-gourmand-projet.alwaysdata.net';
+$dbname   = 'vite-et-gourmand-projet_vite-et-gourmand-projet_bdd';       // Ex: jean-dupond_vite_et_gourmand
+$username = 'vite-et-gourmand-projet';                    // Ton nom d'utilisateur Alwaysdata
+$password = 'VEG-admin123!';                   // Le mot de passe défini dans l'admin Alwaysdata
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+    // En production, on évite d'afficher $e->getMessage() pour des raisons de sécurité
+    die("Erreur de connexion à la base de données.");
 }
